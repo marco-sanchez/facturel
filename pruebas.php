ef0a5f180ed2472fa62ae9d979f4fb9042c70e8a
Marco <marco.sanchez@poiesisconsulting.com>
2014-08-14 18:54:29 -0400

    Fixing styles

--- pruebas.php
+++ pruebas.php
@@ -1,146 +1,56 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>PRUEBAS</title>
+    <title>FACTUREL</title>
     <meta charset="UTF-8"/>
-    <script src="js/_main.js"></script>
-    <style>
-        html{
-            height: 100%;
-        }
-
-        body {
-            height: 95%;
-
-            font-family: "Lucida Console", Monaco, monospace;
-            font-size: 20px;
-
-            color: white;
-
-            background-color: rgb(40, 40, 40);
-        }
-
-        .msgBox{
-            /* CENTER DIV IN SCREEN
-            margin-left = -(width / 2);
-            margin-top: = -(height / 2); */
-            position: absolute;
-            top: 20%;
-            left: 50%;
-            width: 300px;
-            height: 200px;
-            margin-left: -150px;
-            /************************/
-
-            display: none;
-            color:black;
-            background-color: lightgray;
-            border-radius: 10px;
-            padding: 10px;
-        }
-
-        .msgTop{
-            position: absolute;
-            left:0;
-            top: 0;
-            width: 94%;
-            text-align: left;
-            padding: 10px;
-            color: white;
-            font-size: 18px;
-            font-weight: bold;
-        }
-
-        .msgTxt{
-            position: absolute;
-            top: 20%;
-        }
-
-        .msgBottom{
-            position: absolute;
-            left:0;
-            bottom: 0;
-            width: 94%;
-            text-align: right;
-            padding: 10px;
-            color: white;
-            font-size: 15px;
-            font-weight: normal;
-        }
-
-        #msgConfirmar{
-            border: solid 5px #dd7f00;
-        }
-        #msgConfirmar .msgTop, #msgConfirmar .msgBottom{
-            background-color: #dd7f00;
-        }
-
-        #msgError{
-            border: solid 5px darkred;
-        }
-
-        #msgError .msgTop, #msgError .msgBottom{
-            background-color: darkred;
-        }
-
-    </style>
-    <script>
-        $(document).ready(function(){
-            $("#btnPruebaConfirmar").click(function() {
-                msgConfirmar(
-                    "Mi Título de Confirmar",
-                    "Esto es<br/>un ejemplo de Confirmar",
-                    "Aceptar",
-                    "Cancelar",
-                    function(){
-                        alert("presionaste SI");
-                    },
-                    function(){
-                        alert("presionaste NO");
-                    }
-                );
-            });
-
-            $("#btnPruebaError").click(function() {
-                msgError(
-                    "Mi Título de ERROR",
-                    "Esto es<br/>un ejemplo de Error"
-                );
-            });
-        });
-
-        function msgConfirmar(titulo, texto, btnSI, btnNO, funcSI, funcNO){
-            $("#msgError").hide();
-            $("#msgConfirmar").show();
-            $(".msgTl").html(titulo);
-            $(".msgTxt").html(texto);
-            $(".btnSI").html(btnSI);
-            $(".btnNO").html(btnNO);
-            $(".btnSI").off("click").click(function() {
-                $(".msgBox").hide();
-                funcSI();
-            });
-            $(".btnNO").off("click").click(function() {
-                $(".msgBox").hide();
-                funcNO();
-            });
-        }
-
-        function msgError(titulo, texto){
-            $("#msgError").show();
-            $("#msgConfirmar").hide();
-            $(".msgTl").html(titulo);
-            $(".msgTxt").html(texto);
-            $(".btnSI").off("click").click(function() {
-                $(".msgBox").hide();
-            });
-        }
-
-    </script>
+    <link rel="stylesheet" type="text/css" href="css/_main.css">
 </head>
 
 <body>
-<div class="central">
+
+<div id="div_logo">
+    <img src="img/logo_empresa.png" style="width: 60%">
+</div>
+
+<div id="div_top">
+    <div id="top_name_help_out">
+        <span id="top_usr_name">
+            Marco Alejandro Sánchez Melgarejo
+        </span>
+        &nbsp;&nbsp;
+        <button class="opt_button" id="ayuda">Ayuda</button>
+        <button class="opt_button btn_negativo" id="salir">Salir</button>
+    </div>
+
+    <div id="tittle">El Título</div>
+
+    <div id="top_menu" class="horizontal_menu">
+        <ul>
+            <li><a class="round_left selected" href="">Ventas</a></li>
+            <li><a href="productos.php">Productos</a></li>
+            <li><a href="clientes.php">Clientes</a></li>
+            <li><a class="round_right" href="reportes.php">Reportes</a></li>
+        </ul>
+    </div>
+</div>
+
+<div id="div_left" class="round_corners">
+    div_left
+</div>
+
+<div id="div_content" class="round_corners">
+
+</div>
+
+<div id="div_footer">
+    div_footer
+</div>
+
+<!-- ############################################################### -->
+<!-- ################## DIÁLOGOS & POPUPS ########################## -->
+
+    <div class="cover"></div>
+
     <div class="msgBox" id="msgConfirmar">
         <div class="msgTop">
             <span class="msgTl">
@@ -165,10 +75,6 @@
             <button class="btnSI">Aceptar</button>
         </div>
     </div>
-</div>
-
-<button id="btnPruebaConfirmar">Prueba Confirmar</button>
-<button id="btnPruebaError">Prueba Error</button>
 
 </body>
 </html>
